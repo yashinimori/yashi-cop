cc09a63a4f02cbc1e7a16e13ee6a0e2d1eb37a70
FerumFlex <ferumflex@gmail.com>
2019-05-27 12:37:21 +0300

    to prev commit

--- www/bank/migrations/0012_auto_20190527_0919.py
+++ www/bank/migrations/0012_auto_20190527_0919.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('bank', '0010_userprofile'),
+        ('bank', '0011_auto_20190508_0834'),
     ]
 
     operations = [
