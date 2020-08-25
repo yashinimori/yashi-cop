e0abfaac7724acb61e18bdb4e55eb62ee87a011a
Xander Pokhylenko <dev@xander.im>
2020-08-25 13:34:41 +0300

    (feat) improve statuses mediation stage.

--- backend/cop/core/api/serializers/claim.py
+++ backend/cop/core/api/serializers/claim.py
@@ -138,7 +138,7 @@ class ClaimSerializer(serializers.ModelSerializer):
     def set_status(self, status_index=None):
         claim = self.instance
         allocation_rc = ['0017', '0018', '0019', '0020', '0021', '0022', '0023', '0024']
-        if claim.transaction or claim.claim_reason_code.code != '0100':
+        if not claim.status and (claim.transaction or claim.claim_reason_code.code != '0100'):
             mediation_escalation_status = 5
             status_index = mediation_escalation_status
         if claim.claim_reason_code in allocation_rc:
