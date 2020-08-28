15c9564bdc582ed6bd830d77f78bbb5c4068f6ba
Xander Pokhylenko <dev@xander.im>
2020-08-28 13:10:09 +0300

    (feat) Use SES to send emails.

--- backend/cop/logger/admin.py
+++ backend/cop/logger/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from cop.logger.models import LoggerEntry
+
+
+@admin.register(LoggerEntry)
+class LoggerEntry(admin.ModelAdmin):
+    pass
