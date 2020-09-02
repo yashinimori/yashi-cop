18b985e800477865b7f76f897bbc0c8873d211c3
Leon <ln@smartdata.com.ua>
2020-09-02 14:44:19 +0300

    add format timeline

--- frontend/src/app/share/models/timeline-view.model.ts
+++ frontend/src/app/share/models/timeline-view.model.ts
@@ -1,7 +1,17 @@
 import { TimelineRegistration } from './timeline-registration.model';
+import { SpawnSyncOptionsWithStringEncoding } from 'child_process';
 
 export class TimelineView extends TimelineRegistration {
     comments: any;
     documents: any;
     stage_changes: any;
+    create_date_str: string;
+    user: any;
+    create_date: Date;
+    description: string;
+    file: string;
+    id: 31
+    type: string;
+    user_name: string;
+    text: string;
 }
