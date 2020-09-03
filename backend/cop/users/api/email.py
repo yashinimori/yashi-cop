e019b500e95577a340e2b4cf57f1f5e349ddfdfd
Xander Pokhylenko <dev@xander.im>
2020-09-03 18:26:43 +0300

    (fix) CustomActivationEmail template.

--- backend/cop/users/api/email.py
+++ backend/cop/users/api/email.py
@@ -1,9 +1,19 @@
-from djoser.email import ActivationEmail
+from django.contrib.auth.tokens import default_token_generator
+from templated_mail.mail import BaseEmailMessage
+
+from djoser import utils
+from djoser.conf import settings
 
 
-class CustomActivationEmail(ActivationEmail):
+class CustomActivationEmail(BaseEmailMessage):
     template_name = "email/custom_activation.html"
 
     def get_context_data(self):
+        # ActivationEmail can be deleted
         context = super().get_context_data()
+
+        user = context.get("user")
+        context["uid"] = utils.encode_uid(user.pk)
+        context["token"] = default_token_generator.make_token(user)
+        context["url"] = settings.ACTIVATION_URL.format(**context)
         return context
