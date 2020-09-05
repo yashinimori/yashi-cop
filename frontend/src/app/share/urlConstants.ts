05085fb69e1ba7a8ffb64853f6eb3e818536f6d9
Xander Pokhylenko <dev@xander.im>
2020-09-05 14:28:43 +0300

    (fix) claim pdf pan.

--- frontend/src/app/share/urlConstants.ts
+++ frontend/src/app/share/urlConstants.ts
@@ -1,5 +1,5 @@
 //const MAIN_URL = 'http://18.185.111.248';
-export const MAIN_URL = ' https://api0.chargebackoptimizer.com';
+export const MAIN_URL = 'https://api0.chargebackoptimizer.com';
 
 export const URL_CREATE_TOKEN = `${MAIN_URL}/api/v1/auth/jwt/create`;
 export const URL_LOGIN = `${MAIN_URL}/api/v1/users/me`;
