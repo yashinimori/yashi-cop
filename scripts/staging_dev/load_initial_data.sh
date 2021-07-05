64821eb4070d1a00c512f1d7d49c41a05a20eb6b
yashinimori <yashinimori@gmail.com>
2021-07-05 18:33:30 +0300

    [Feat] Added correct initial data

--- scripts/staging_dev/load_initial_data.sh
+++ scripts/staging_dev/load_initial_data.sh
@@ -5,3 +5,8 @@ docker-compose -f staging_dev.yml run --rm django ./manage.py loaddata cop/fixtu
 docker-compose -f staging_dev.yml run --rm django ./manage.py loaddata cop/fixtures/survey_questions.json
 docker-compose -f staging_dev.yml run --rm django ./manage.py loaddata cop/fixtures/system_user.json
 docker-compose -f staging_dev.yml run --rm django ./manage.py loaddata cop/fixtures/test_chargeback_officer.json
+docker-compose -f staging_dev.yml run --rm django ./manage.py loaddata cop/fixtures/cc_branches.json
+docker-compose -f staging_dev.yml run --rm django ./manage.py loaddata cop/fixtures/ch_users.json
+docker-compose -f staging_dev.yml run --rm django ./manage.py loaddata cop/fixtures/security_users.json
+docker-compose -f staging_dev.yml run --rm django ./manage.py loaddata cop/fixtures/test_chargeback_officer.json
+docker-compose -f staging_dev.yml run --rm django ./manage.py loaddata cop/fixtures/top_users.json
